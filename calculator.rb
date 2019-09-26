4b694eb432f40dbac60c8a308dcd1a7410a8c1c6
Logan Smith <logsmith06@gmail.com>
2019-09-26 00:45:11 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,27 @@
 # Add your variables here
+
+
+first_number = 5
+second_number = 1
+
+#sum method
+def add_them_up(first_number, second_number)
+  sum = first_number + second_number
+end
+
+#difference method
+def subtract_them(first_number, second_number)
+  difference = first_number - second_number
+end
+
+#product method
+def multiply_them(first_number, second_number)
+  product = first_number * second_number
+end
+
+#quotient method
+def divide_them(first_number, second_number)
+  quotient = first_number / second_number
+end
+
+
