0997ede1cd487162bbfab8da3bfcc24f81169919
Logan Smith <logsmith06@gmail.com>
2019-09-26 00:46:21 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -8,18 +8,24 @@ second_number = 1
 def add_them_up(first_number, second_number)
   sum = first_number + second_number
 end
+add_them_up
 
 #difference method
 def subtract_them(first_number, second_number)
   difference = first_number - second_number
 end
+subtract_them
 
 #product method
 def multiply_them(first_number, second_number)
   product = first_number * second_number
 end
+multiply_them
 
 #quotient method
 def divide_them(first_number, second_number)
   quotient = first_number / second_number
 end
+divide_them
+
+
